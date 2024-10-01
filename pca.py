6463d851000910565328c4da2efd1cf62a160b09
Stephen Nicholas Swatman <stephen@v25.nl>
2024-10-01 07:59:58 +0200

    Updates

--- pca.py
+++ pca.py
@@ -1,38 +1,28 @@
-import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
+import numpy
+import matplotlib.pyplot
 from sklearn.decomposition import PCA
+import data
 
-votes = np.array([
-    [-1, -1, -1, 1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1, -1, 1, 1, -1, 1, -1, 1, 1, -1, 1, -1, -1, 1, -1, -1, -1],
-    [1, 1, -1, 1, -1, 1, -1, -1, -1, 1, -1, -1, -1, 1, -1, 1, 1, 1, 1, -1, 1, 1, -1, 0, 1, -1, -1, -1, -1, 1],
-    [1, 0, 1, -1, 1, 1, -1, 0, -1, 1, 1, 1, -1, -1, 0, 1, 1, 1, 1, -1, -1, -1, -1, 1, 0, 1, 1, -1, 1, -1],
-    [-1, -1, -1, -1, 1, -1, -1, 1, 1, -1, 1, 1, 0, -1, 1, -1, -1, 1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, 1, -1],
-    [-1, 1, -1, -1, -1, 1, -1, -1, 1, -1, 1, 1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, -1, -1, -1, -1, 1, -1, 1, -1],
-    [1, -1, 1, -1, 1, -1, -1, 1, 1, -1, 1, -1, 1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, 0, -1, 1, 1, -1],
-    [-1, 1, -1, -1, -1, 1, 1, -1, 1, -1, -1, -1, -1, 0, -1, 1, 1, -1, -1, 1, 1, 1, -1, -1, -1, -1, 1, -1, 1, -1],
-    [-1, 1, 1, -1, 1, 1, -1, 1, -1, -1, 1, -1, 1, -1, 1, -1, 1, 1, 1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, -1],
-    [1, -1, -1, 1, 1, 1, 1, -1, 1, -1, -1, 1, -1, 1, -1, 1, 1, 1, 1, 1, 1, 1, -1, 1, -1, 0, 1, -1, -1, 1],
-    [1, -1, 1, -1, 1, -1, -1, -1, 1, -1, -1, -1, 1, -1, 1, -1, -1, 0, 1, 1, -1, -1, -1, 1, -1, -1, -1, 1, 1, -1],
-    [-1, 1, -1, -1, -1, 1, 1, -1, 1, -1, 1, 1, -1, -1, -1, 1, 1, 0, 1, -1, 1, 0, -1, -1, -1, -1, 1, -1, -1, -1],
-    [1, -1, -1, -1, 1, -1, -1, 1, -1, 1, 1, 1, 1, -1, 1, -1, -1, 1, -1, 1, -1, -1, -1, -1, 1, 1, -1, -1, 1, -1],
-    [1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1, 1, 1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, 1, -1, -1, 1, -1]
-])
+dims = 2
 
-names = ["VVD", "PVV", "50Plus", "Denk", "ChristenUnie", "GroenLinks", "SGP", "PvdA", "FvD", "D66", "CDA", "SP", "PvdD"]
+pca = PCA(n_components=dims)
+pca.fit(data.votes)
+fit = pca.transform(data.votes)
 
-pca = PCA(n_components=3)
-pca.fit(votes)
-data = pca.transform(votes)
+fig = matplotlib.pyplot.figure()
+if dims == 3:
+    ax = fig.add_subplot(projection='3d')
+    ax.set_title('3D PCA Nederlandse Politiek')
+    ax.scatter(fit[:,0], fit[:,1], fit[:,2])
+    for i, txt in enumerate(data.names):
+        ax.text(fit[i,0],fit[i,1], fit[i,2], txt)
+elif dims == 2:
+    ax = fig.add_subplot()
+    ax.set_title('2D PCA Nederlandse Politiek')
+    ax.scatter(fit[:,0], fit[:,1])
+    for i, txt in enumerate(data.names):
+        ax.text(fit[i,0],fit[i,1], txt)
+else:
+    raise ValueError("Must be 2D or 3D.")
 
-print(data)
-
-fig = plt.figure()
-ax = Axes3D(fig)
-ax.set_title('3D PCA Nederlandse Politiek')
-ax.scatter(data[:,0], data[:,1], data[:,2])
-
-for i, txt in enumerate(names):
-    ax.text(data[i,0],data[i,1], data[i,2], txt)
-
-plt.show()
+matplotlib.pyplot.show()
